248387cea6246c1ca03187232d996a1ae6c00f80
Zaccary Price <wildtangomedia@gmail.com>
2013-10-05 23:03:39 +0300

    added prism syntax highlighting

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,49 +1,39 @@
-window.scrollTo(0,1);
+window.scrollTo(0, 1);
 
-$('.nav-open').click(function(e) {
-    e.preventDefault();
-    $('.site-nav').toggleClass('active');
-    $(this).toggleClass('active');
-});
-
-function stickyTopLink()
-{
-    var y = ($(window).scrollTop() + $(window).innerHeight()) - ($(".top-link").height())* 2,
-        top = $(window).scrollTop();
-
-    if(top > $(".site-nav").height())
-    {
-        $(".top-link").css({
-            'top': y,
-            'right': $(".top-link").width()
-        }).removeClass('hidden');
-    }
-    else
-    {
-        $(".top-link").addClass('hidden');
-    }
-}
-
-$(window).scroll(stickyTopLink);
-$(window).resize(stickyTopLink);
-
-$(".top-link").click(function(e){
-    $('html, body').animate({
-        scrollTop: 0
-    }, '250');
+$('.nav-open').click(function (e) {
+	e.preventDefault();
+	$('.site-nav').toggleClass('active');
+	$(this).toggleClass('active');
 });
 
 
-$('#contact-form').submit(function(e){
-    e.preventDefault();
-    $(this).find('button[type="submit"]').attr('disable', 'true').html('Sending... <i class="icon-refresh icon-spin"></i>');
-    $.post('/assets/scripts/post-message.php', $(this).serialize(), function(data){
-        console.log(data);
-        console.log($(this));
-        if (data.success !== undefined) {
-            $('#contact-form').find('button[type="submit"]').html('Sent! <i class="icon-tick"></i>');
-        } else {
-            $('#contact-form').find('button[type="submit"]').addClass('red').removeClass('cta').html('Something went wrong ): <i class="icon-cross"></i>');
-        }
-    }, 'json');
+$(window).scroll(function (e) {
+	var top = $(window).innerHeight(),
+		y = top + $(window).scrollTop() - ($(".top-link").height()) * 2;
+
+	$(".top-link").css({
+		'top': y,
+		'right': $(".top-link").width()
+	}).removeClass('hidden');
+});
+
+$(".top-link").click(function (e) {
+	$('html, body').animate({
+		scrollTop: 0
+	}, '250');
+});
+
+
+$('#contact-form').submit(function (e) {
+	e.preventDefault();
+	$(this).find('button[type="submit"]').attr('disable', 'true').html('Sending... <i class="icon-refresh icon-spin"></i>');
+	$.post('/assets/scripts/post-message.php', $(this).serialize(), function (data) {
+		console.log(data);
+		console.log($(this));
+		if (data.success !== undefined) {
+			$('#contact-form').find('button[type="submit"]').html('Sent! <i class="icon-tick"></i>');
+		} else {
+			$('#contact-form').find('button[type="submit"]').addClass('red').removeClass('cta').html('Something went wrong ): <i class="icon-cross"></i>');
+		}
+	}, 'json');
 });
